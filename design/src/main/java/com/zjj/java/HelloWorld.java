6428a8190f0225685ab8dd733801198d9405de20
QuietClickCode <2572417548@qq.com>
2020-02-03 21:14:39 +0800

    初始化

---
+++ design/src/main/java/com/zjj/java/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.zjj.java;
+
+/**
+ * @program: DesignPattern
+ * @description: HelloWorld
+ * @author: zjj
+ * @create: 2020-02-03 20:23:51
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello,World!");
+    }
+}
